3d6bec7dda2a2e97562d618359236a70450a760c
Magyt71 <mhmg8793@gmail.com>
2026-04-03 00:28:31 +0200

    updating the code

--- go.mod
+++ go.mod
@@ -1,9 +1,20 @@
-module my-discord-bot
+module Orginzation_app
 
 go 1.25.0
 
 require (
+	fyne.io/fyne/v2 v2.7.3 // indirect
+	git.sr.ht/~jackmordaunt/go-toast v1.1.2 // indirect
+	github.com/esiqveland/notify v0.13.3 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/gen2brain/beeep v0.11.2 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/jackmordaunt/icns/v3 v3.0.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/sergeymakinen/go-bmp v1.0.0 // indirect
+	github.com/sergeymakinen/go-ico v1.0.0-beta.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
-	"github.com/joho/godotenv"
+	"github.com/gen2brain/beeep"
 )
 
+var HomDir, _ = os.UserHomeDir()
+
+// the extension and where it go
 var theMap = map[string]string{
 	".jpg": "Image", ".png": "Image", ".jpeg": "Image",
 	".gif": "Image", ".svg": "Image", ".webp": "Image",
@@ -24,20 +29,18 @@ var theMap = map[string]string{
 
 func main() {
 
-	HomDir, _ := os.UserHomeDir()
 	thepath := filepath.Join(HomDir, "Downloads")
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
+	// Creating the watcher
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer watcher.Close()
 
+	var PrograssingFile sync.Map
+
+	// making loop for the watcher
 	go func() {
 		for {
 			select {
@@ -47,9 +50,20 @@ func main() {
 				}
 				log.Println("event:", event)
 				if event.Has(fsnotify.Write) || event.Has(fsnotify.Create) {
-					log.Println("modified file:", event.Name)
-					go MoveFile(event.Name)
+
+					if _, IsProssing := PrograssingFile.LoadOrStore(event.Name, true); !IsProssing {
+
+						log.Println("New file detected:", event.Name)
+
+						go func(FileName string) {
+							log.Println("modified file:", FileName)
+							MoveFile(FileName)
+
+							PrograssingFile.Delete(FileName)
+						}(event.Name)
+					}
 				}
+
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
@@ -58,6 +72,7 @@ func main() {
 			}
 		}
 	}()
+
 	err = watcher.Add(thepath)
 	if err != nil {
 		log.Println(err)
@@ -67,19 +82,66 @@ func main() {
 	<-done
 }
 
+// for make sure it's don't delete anyfile with the same name
+func UniqPath(FilePath string) string {
+	Dir := filepath.Dir(FilePath)
+	FileName := filepath.Base(FilePath)
+	Ext := filepath.Ext(FilePath)
+	NameOnly := strings.TrimSuffix(FileName, Ext)
+
+	re := regexp.MustCompile(`\(\d+\)$`)
+	CleanName := re.ReplaceAllString(NameOnly, "")
+
+	FinalPath := FilePath
+	count := 1
+
+	for {
+		if _, err := os.Stat(FinalPath); os.IsNotExist(err) {
+			break
+		}
+
+		NewName := fmt.Sprintf("%s(%d)%s", CleanName, count, Ext)
+		FinalPath = filepath.Join(Dir, NewName)
+		count++
+	}
+	return FinalPath
+}
+
+// Moving the file to the target folder
 func MoveFile(FilePath string) {
+
+	if _, err := os.Stat(FilePath); os.IsNotExist(err) {
+		return
+	}
+
 	extension := strings.ToLower(filepath.Ext(FilePath))
-
-	HomDir, _ := os.UserHomeDir()
-
 	Targetfile, exists := theMap[extension]
+
 	if exists {
-		time.Sleep(5 * time.Second)
+		time.Sleep(10 *time.Second)
+		FileName := filepath.Base(FilePath)
+		beeep.Notify("we found a file to move it", FileName, "just waiting to make it done")
+
+		for {
+			// for make sure it's done downloading
+			TheFile, err := os.OpenFile(FilePath, os.O_RDWR, 0)
+			if err == nil {
+				TheFile.Close()
+				break
+			}
+			log.Println("still downloading")
+			time.Sleep(20 * time.Minute)
+		}
+
+		if _, err := os.Stat(FilePath); os.IsNotExist(err) {
+			return
+		}
+
 		Newpath := filepath.Join(HomDir, "Downloads", Targetfile)
 		os.MkdirAll(Newpath, os.ModePerm)
 
 		filename := filepath.Base(FilePath)
-		finalpath := filepath.Join(Newpath, filename)
+		finalpath := UniqPath(filepath.Join(Newpath, filename))
 
 		err := os.Rename(FilePath, finalpath)
 		if err != nil {
@@ -87,8 +149,6 @@ func MoveFile(FilePath string) {
 		} else {
 			log.Println("the file pass", Targetfile)
 		}
-	}
-	if !exists {
-
+		beeep.Alert("the file has moved", FileName, "successfully")
 	}
 }
